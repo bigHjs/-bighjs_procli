9e9d28b4db9eb5c4662f55497fe6394b74c4ba31
bighjs <82922939+bigHjs@users.noreply.github.com>
2024-10-10 09:29:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@
 const baseURL = "https://api.github.com";
 // organization 名称
 const users = "bigHjs";
-// token
+// token https://github.com/settings/tokens/new 申请以及设置可用时间
 const token =
-  "N O e 8 o O O q N B U F G A 5 G e M R Y k F S h b z t z 1 P 0 e f y P j h 1 Y W Q O f k 6 y a z A F t T R Q F Y g q o _ 6 I N f k S o i w b j T 0 Y O 3 U Y T A 1 1 _ t a p _ b u h t i g";
+  "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";
 
 // repos list 库列表
 const reposList = {};
