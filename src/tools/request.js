9eb500351bfa4c1972beb993e10bc570f7ac2526
bighjs <82922939+bigHjs@users.noreply.github.com>
2024-10-10 09:29:02 +0800

    Update request.js

--- src/tools/request.js
+++ src/tools/request.js
@@ -10,7 +10,7 @@ const get = function (url) {
           "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
           "X-GitHub-Api-Version": "2022-11-28",
-          Authorization: `token ${token.split(" ").reverse().join("")}`,
+          Authorization: `token ${token}`,
         },
       },
       (err, response, body) => {
